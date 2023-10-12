0fed6287dc820ef31fdbd62b0cb4a30d41f7c66e
Watersong89 <watersongordon@gmail.com>
2023-10-12 16:07:47 +0100

    Added alert function to get player choice

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 let computerChoice = getComputerChoice();
-const playerSelection = 'Rock';
+const playerSelection = prompt('Make your choice!').toLowerCase;
 
 function getComputerChoice() {
     let randomNumber = Math.random();
     let result = '';
     if(randomNumber <= 0.33) {
-        result = 'Rock';
+        result = 'rock';
     } else if (randomNumber > 0.33 && randomNumber < 0.66) {
-        result = 'Paper';
+        result = 'paper';
     } else {
-        result = 'Scissors';
+        result = 'scissors';
     }
     return result;
 }
 
 function playRound(playerSelection, computerChoice) {
     let result = '';
-    if (computerChoice === 'Rock') {
-        if (playerSelection === 'Rock') {
+    if (computerChoice === 'rock') {
+        if (playerSelection === 'rock') {
             result = 'Draw!';
-        } else if (playerSelection === 'Paper') {
+        } else if (playerSelection === 'paper') {
             result = 'You Win! Paper beats Rock!';
         } else {
             result = 'You Lose! Rock beats Scissors!';
         };
-    }else if (computerChoice === 'Paper') {
-        if (playerSelection === 'Rock') {
+    }else if (computerChoice === 'paper') {
+        if (playerSelection === 'rock') {
             result = 'You lose! Paper beats Rock!';
-        } else if (playerSelection === 'Paper') {
+        } else if (playerSelection === 'paper') {
             result = 'Draw!';
         } else {
             result = 'You Win! Scissors beat Paper!';
         };
-    }else if (computerChoice === 'Scissors') {
-        if (playerSelection === 'Rock') {
+    }else if (computerChoice === 'scissors') {
+        if (playerSelection === 'sock') {
             result = 'You Win! Rock beats Scissors!';
-        } else if (playerSelection === 'Paper') {
+        } else if (playerSelection === 'paper') {
             result = 'You lose! Scissors beat Paper!';
         } else {
             result = 'Draw';
