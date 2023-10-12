ee8f7f6e352d16d482279504d658926cbaa6ae16
Watersong89 <watersongordon@gmail.com>
2023-10-12 16:02:13 +0100

    Created playRound function with game logic

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
-let computerChoice;
+let computerChoice = getComputerChoice();
+const playerSelection = 'Rock';
 
 function getComputerChoice() {
     let randomNumber = Math.random();
+    let result = '';
     if(randomNumber <= 0.33) {
-        computerChoice = 'Rock';
+        result = 'Rock';
     } else if (randomNumber > 0.33 && randomNumber < 0.66) {
-        computerChoice = 'Paper';
+        result = 'Paper';
     } else {
-        computerChoice = 'Scissors';
+        result = 'Scissors';
     }
-    return computerChoice;
+    return result;
 }
 
 function playRound(playerSelection, computerChoice) {
-
-
+    let result = '';
+    if (computerChoice === 'Rock') {
+        if (playerSelection === 'Rock') {
+            result = 'Draw!';
+        } else if (playerSelection === 'Paper') {
+            result = 'You Win! Paper beats Rock!';
+        } else {
+            result = 'You Lose! Rock beats Scissors!';
+        };
+    }else if (computerChoice === 'Paper') {
+        if (playerSelection === 'Rock') {
+            result = 'You lose! Paper beats Rock!';
+        } else if (playerSelection === 'Paper') {
+            result = 'Draw!';
+        } else {
+            result = 'You Win! Scissors beat Paper!';
+        };
+    }else if (computerChoice === 'Scissors') {
+        if (playerSelection === 'Rock') {
+            result = 'You Win! Rock beats Scissors!';
+        } else if (playerSelection === 'Paper') {
+            result = 'You lose! Scissors beat Paper!';
+        } else {
+            result = 'Draw';
+        }; 
+    }
+    return result;
 }
 
-console.log(getComputerChoice();)
+console.log(playRound(playerSelection, computerChoice));
