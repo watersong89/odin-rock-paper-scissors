fd94b9708a831866259162c22e1380c2f377d459
Watersong89 <watersongordon@gmail.com>
2023-10-12 17:18:42 +0100

    extrapolated inner functions from playGame()

--- script.js
+++ script.js
@@ -3,65 +3,63 @@ let computerScore = 0;
     let draws = 0;
 
 function playGame() {
-
-    function getComputerChoice() {
-        let randomNumber = Math.random();
-        let result = '';
-        if(randomNumber <= 0.33) {
-            result = 'rock';
-        } else if (randomNumber > 0.33 && randomNumber < 0.66) {
-            result = 'paper';
-        } else {
-            result = 'scissors';
-        }
-        return result;
-    }
-
     const playerSelection = prompt('Make your choice!').toLowerCase();
     const computerChoice = getComputerChoice();
-
-    function playRound(playerSelection, computerChoice) {
-        let result = '';
-        if (computerChoice === 'rock') {
-            if (playerSelection === 'rock') {
-                result = 'Draw!';
-                draws++;
-            } else if (playerSelection === 'paper') {
-                result = `You Win! ${playerSelection} beats ${computerChoice}!`;
-                playerScore++;
-            } else {
-                result = `You Lose! ${computerChoice} beats ${playerSelection}!`;
-                computerScore++;
-            };
-        }else if (computerChoice === 'paper') {
-            if (playerSelection === 'rock') {
-                result = `You Lose! ${computerChoice} beats ${playerSelection}!`;
-                computerScore++;
-            } else if (playerSelection === 'paper') {
-                result = 'Draw!';
-                draws++
-            } else {
-                result = `You Win! ${playerSelection} beats ${computerChoice}!`;
-                playerScore++;
-            };
-        }else if (computerChoice === 'scissors') {
-            if (playerSelection === 'rock') {
-                result = `You Win! ${playerSelection} beats ${computerChoice}!`;
-                playerScore++;
-            } else if (playerSelection === 'paper') {
-                result = `You Lose! ${computerChoice} beats ${playerSelection}!`;
-                computerScore++;
-            } else {
-                result = 'Draw!';
-                draws++
-            }; 
-        }
-        return result;
-    }
-  
     console.log(playRound(playerSelection, computerChoice));
     const score = `Computer: ${computerScore}, Player: ${playerScore}, Draws: ${draws}`;
     return score;
+}
+
+function playRound(playerSelection, computerChoice) {
+    let result = '';
+    if (computerChoice === 'rock') {
+        if (playerSelection === 'rock') {
+            result = 'Draw!';
+            draws++;
+        } else if (playerSelection === 'paper') {
+            result = `You Win! ${playerSelection} beats ${computerChoice}!`;
+            playerScore++;
+        } else {
+            result = `You Lose! ${computerChoice} beats ${playerSelection}!`;
+            computerScore++;
+        };
+    }else if (computerChoice === 'paper') {
+        if (playerSelection === 'rock') {
+            result = `You Lose! ${computerChoice} beats ${playerSelection}!`;
+            computerScore++;
+        } else if (playerSelection === 'paper') {
+            result = 'Draw!';
+            draws++
+        } else {
+            result = `You Win! ${playerSelection} beats ${computerChoice}!`;
+            playerScore++;
+        };
+    }else if (computerChoice === 'scissors') {
+        if (playerSelection === 'rock') {
+            result = `You Win! ${playerSelection} beats ${computerChoice}!`;
+            playerScore++;
+        } else if (playerSelection === 'paper') {
+            result = `You Lose! ${computerChoice} beats ${playerSelection}!`;
+            computerScore++;
+        } else {
+            result = 'Draw!';
+            draws++
+        }; 
+    }
+    return result;
+}
+
+function getComputerChoice() {
+    let randomNumber = Math.random();
+    let result = '';
+    if(randomNumber <= 0.33) {
+        result = 'rock';
+    } else if (randomNumber > 0.33 && randomNumber < 0.66) {
+        result = 'paper';
+    } else {
+        result = 'scissors';
+    }
+    return result;
 }
 
 for (let i = 0; i < 5; i++) {
