683003846df9316faf8da971ed0eef8cf33326b7
Watersong89 <watersongordon@gmail.com>
2023-10-12 15:35:10 +0100

    Created function to get computer choice

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
+function getComputerChoice() {
+    let randomNumber = Math.random();
+    let computerChoice;
+    if(randomNumber <= 0.33) {
+        computerChoice = 'Rock';
+    } else if (randomNumber > 0.33 && randomNumber < 0.66) {
+        computerChoice = 'Paper';
+    } else {
+        computerChoice = 'Scissors';
+    }
+    return computerChoice;
+}
+
 
 
