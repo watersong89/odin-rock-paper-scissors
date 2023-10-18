4627fcf812c8419ed56db83bb0c1e5520af867de
Watersong89 <watersongordon@gmail.com>
2023-10-18 15:48:03 +0100

    Capitalised 'Rock' 'Paper' and 'Scissors'

--- script.js
+++ script.js
@@ -6,33 +6,33 @@ let messageElement = document.querySelector('.js-result-message');
 
 function playRound(playerSelection, computerChoice) {
     let result = '';
-    if (computerChoice === 'rock') {
-        if (playerSelection === 'rock') {
+    if (computerChoice === 'Rock') {
+        if (playerSelection === 'Rock') {
             result = 'Draw!';
             draws++;
-        } else if (playerSelection === 'paper') {
+        } else if (playerSelection === 'Paper') {
             result = `You Win! ${playerSelection} beats ${computerChoice}!`;
             playerScore++;
         } else {
             result = `You Lose! ${computerChoice} beats ${playerSelection}!`;
             computerScore++;
         };
-    } else if (computerChoice === 'paper') {
-        if (playerSelection === 'rock') {
+    } else if (computerChoice === 'Paper') {
+        if (playerSelection === 'Rock') {
             result = `You Lose! ${computerChoice} beats ${playerSelection}!`;
             computerScore++;
-        } else if (playerSelection === 'paper') {
+        } else if (playerSelection === 'Paper') {
             result = 'Draw!';
             draws++
         } else {
             result = `You Win! ${playerSelection} beats ${computerChoice}!`;
             playerScore++;
         };
-    } else if (computerChoice === 'scissors') {
-        if (playerSelection === 'rock') {
+    } else if (computerChoice === 'Scissors') {
+        if (playerSelection === 'Rock') {
             result = `You Win! ${playerSelection} beats ${computerChoice}!`;
             playerScore++;
-        } else if (playerSelection === 'paper') {
+        } else if (playerSelection === 'Paper') {
             result = `You Lose! ${computerChoice} beats ${playerSelection}!`;
             computerScore++;
         } else {
@@ -54,11 +54,11 @@ function getComputerChoice() {
     let randomNumber = Math.random();
     let result = '';
     if (randomNumber <= 0.33) {
-        result = 'rock';
+        result = 'Rock';
     } else if (randomNumber > 0.33 && randomNumber < 0.66) {
-        result = 'paper';
+        result = 'Paper';
     } else {
-        result = 'scissors';
+        result = 'Scissors';
     }
     return result;
 }
@@ -69,15 +69,15 @@ const scissorsButton = document.querySelector('.js-button-scissors');
 const resetButton = document.querySelector('.js-reset-button');
 
 rockButton.addEventListener('click', () => {
-    playRound('rock', getComputerChoice());
+    playRound('Rock', getComputerChoice());
 });
 
 paperButton.addEventListener('click', () => {
-    playRound('paper', getComputerChoice());
+    playRound('Paper', getComputerChoice());
 });
 
 scissorsButton.addEventListener('click', () => {
-    playRound('scissors', getComputerChoice());
+    playRound('Scissors', getComputerChoice());
 });
 
 resetButton.addEventListener('click', () => {
